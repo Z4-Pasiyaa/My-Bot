5f4a0d2990f089a06a89cdc0808bf310fc350475
Z4-Pasiyaa <155916805+Z4-Pasiyaa@users.noreply.github.com>
2024-01-08 01:57:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,10 @@ const keep_alive = require('./keep_alive.js')
 const client = new Discord.Client();
 const { Client , IntentsBitField, Integration, EmbedBuilder, ActivityType, Options } = require('discord.js');
 
-let status = [
-  {
-    name: "Z4 Army Discord",
-    type: ActivityType.Watching,
-  },
-  {
-    name: "illustrator",
-    type: ActivityType.Playing,
-  },
-  {
-    name: "Visual Studio Code",
-    type: ActivityType.Playing,
-  },
-  {
-    name: "Photoshop",
-    type: ActivityType.Playing,
-  },
-];
 
 client.on("ready", (c) => {
   console.log(`${c.user.username} is Online.✅`);
 
-  setInterval(() => {
-    let random = Math.floor(Math.random() * status.length);
-    client.user.setActivity(status[random]);
-  }, 10000);
-});
 
 
 client.login(process.env.TOKEN); //login bot using token
