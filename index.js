d1989a6177f58bf555f0677d562a08dc5a7fb4e2
Z4-Pasiyaa <155916805+Z4-Pasiyaa@users.noreply.github.com>
2024-01-08 01:39:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js'); //import client from discord
+const keep_alive = require('./keep_alive.js')
+const client = new Discord.Client();
+
+let status = [
+  {
+    name: "Z4 Army Discord",
+    type: ActivityType.Watching,
+  },
+  {
+    name: "illustrator",
+    type: ActivityType.Playing,
+  },
+  {
+    name: "Visual Studio Code",
+    type: ActivityType.Playing,
+  },
+  {
+    name: "Photoshop",
+    type: ActivityType.Playing,
+  },
+];
+
+client.on("ready", (c) => {
+  console.log(`${c.user.username} is Online.✅`);
+
+  setInterval(() => {
+    let random = Math.floor(Math.random() * status.length);
+    client.user.setActivity(status[random]);
+  }, 10000);
+});
+
+
+client.login(process.env.TOKEN); //login bot using token
